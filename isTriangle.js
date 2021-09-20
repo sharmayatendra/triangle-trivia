aabee6e6c2841f1fef36bcf42e0606bc010dd215
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2021-09-20 13:56:20 +0530

    update: angles logic

--- isTriangle.js
+++ isTriangle.js
@@ -3,15 +3,18 @@ const isTriangleButton = document.querySelector("#is-triangle");
 const outputDiv = document.querySelector("#output-div");
 
 function calculateSumOfAngles(angle1, angle2, angle3) {
-   const sumOfAngles = angle1 + angle2 + angle3;
+   const sumOfAngles = (angle1 + angle2 + angle3);
    return sumOfAngles;
 } 
 
 function isTriangle() {
-    const sumOfAngles = calculateSumOfAngles(Number(anglesOfTriangle[0].value), Number(anglesOfTriangle[1].value), Number(anglesOfTriangle[2].value))
-    if(sumOfAngles === 180) {
-        outputDiv.innerText = "Angles form a triangle";
-    } 
+    if(Number(anglesOfTriangle[0].value) > 0 && Number(anglesOfTriangle[1].value) > 0 && Number(anglesOfTriangle[2].value) > 0 ) {
+        const sumOfAngles = calculateSumOfAngles(Number(anglesOfTriangle[0].value), Number(anglesOfTriangle[1].value), Number(anglesOfTriangle[2].value))
+        if(sumOfAngles === 180) {
+            outputDiv.innerText = "Angles form a triangle";
+        } 
+    }
+
     else {
     outputDiv.innerText = "Not a triangle";
     }
