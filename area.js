ac0ac47e3d0546510e146729d6a57a7253a88ca5
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2021-09-21 02:14:51 +0530

    style: added stylesheet

--- area.js
+++ area.js
@@ -3,7 +3,7 @@ const calculateAreaBtn = document.querySelector("#area-btn");
 const outputDiv = document.querySelector("#output-div");
 
 function calculateArea() {
-    if(Number(sides[0].value) < 0 || Number(sides[1].value < 0)){
+    if(Number(sides[0].value) <= 0 || Number(sides[1].value <= 0)){
         outputDiv.innerText = "please enter valid side length!";
     }else {
         const area = 1/2 * Number(sides[0].value) * Number(sides[1].value);
--- isTriangle.js
+++ isTriangle.js
@@ -8,16 +8,17 @@ function calculateSumOfAngles(angle1, angle2, angle3) {
 } 
 
 function isTriangle() {
-    if(Number(anglesOfTriangle[0].value) > 0 && Number(anglesOfTriangle[1].value) > 0 && Number(anglesOfTriangle[2].value) > 0 ) {
+    if(Number(anglesOfTriangle[0].value) > 0 && Number(anglesOfTriangle[1].value) > 0 && Number(anglesOfTriangle[2].value) > 0 && Number(anglesOfTriangle[0].value) != 0 && Number(anglesOfTriangle[1].value) != 0 && Number(anglesOfTriangle[2].value) != 0) {
         const sumOfAngles = calculateSumOfAngles(Number(anglesOfTriangle[0].value), Number(anglesOfTriangle[1].value), Number(anglesOfTriangle[2].value))
         if(sumOfAngles === 180) {
             outputDiv.innerText = "Angles form a triangle";
-        } 
+        } else {
+            outputDiv.innerText = "Not a triangle";
+            }
     }
-
     else {
-    outputDiv.innerText = "Not a triangle";
-    }
+        outputDiv.innerText = "plz enter valid input";
+        }
 }
 
 isTriangleButton.addEventListener("click", isTriangle)
